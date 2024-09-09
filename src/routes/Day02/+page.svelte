d1bc09a4c72eac277f99fd275018dd9778616b87
ToddLiao <toddliao.dev@gmail.com>
2024-09-10 00:14:43 +0800

    feat: modify root page

---
+++ src/routes/Day02/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Title from './Title.svelte';
+	const day = 2;
+</script>
+
+<Title />
+<h1>
+	Day {day}
+</h1>
+<div class="content">
+	今天 IT 鐵人賽第 {day} 天，我們來看看 svelte 怎麼用吧！
+</div>
+
+<style scoped>
+	.content {
+		color: #ddd;
+	}
+</style>
---
+++ src/routes/Day02/Title.svelte
@@ -0,0 +1,5 @@
+<script>
+	const day = 2;
+</script>
+
+<h1>Day {day}</h1>
