e606af6606f169057816b8f8e7246e87c15f207f
ToddLiao <toddliao.dev@gmail.com>
2024-09-10 00:15:12 +0800

    feat: modify root page (#1)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	const name = 'Todd';
+</script>
+
+<h1>Hi, {name}! Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	h1 {
+		color: cadetblue;
+	}
+</style>
