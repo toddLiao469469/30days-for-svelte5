4ba732a2b451b3f66958341067829244a971c10c
ToddLiao <toddliao.dev@gmail.com>
2024-09-16 21:12:17 +0800

    fix: modify day02

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
 	const name = 'Todd';
+
+	const handleClick = (name: string) => {
+		console.log(`Hello, ${name}!`);
+	};
 </script>
 
-<h1>Hi, {name}! Welcome to SvelteKit</h1>
+<h1 id="title">Hi, <span class="name">{name}</span>! Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<style>
-	h1 {
+<button onclick={() => handleClick(name)}>Click me</button>
+
+<style lang="scss">
+	:global(body) {
+		background-color: black;
+	}
+	#title {
+		font-weight: 600;
+		color: white;
+	}
+
+	#title .name {
 		color: cadetblue;
+	}
+
+	p {
+		color: #ddd;
 	}
 </style>
