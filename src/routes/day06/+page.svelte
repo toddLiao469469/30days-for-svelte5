afb9ffd519166dbfe7bf8e5faafa7541ea62ef3d
ToddLiao <toddliao.dev@gmail.com>
2024-09-20 23:31:21 +0800

    feat: add day06

---
+++ src/routes/day06/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Counter from './Counter.svelte';
+
+	let showCounter = $state(true);
+</script>
+
+<div>
+	<button
+		onclick={() => {
+			showCounter = !showCounter;
+		}}
+	>
+		{showCounter ? 'Hide' : 'Show'} Counter
+	</button>
+</div>
+{#if showCounter}
+	<Counter></Counter>
+{/if}
+
+<style>
+</style>
