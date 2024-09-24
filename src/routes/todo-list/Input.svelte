13cc6f9f0ef936c1271fd99f24e3790c6f1dfe75
ToddLiao <toddliao.dev@gmail.com>
2024-09-24 23:37:03 +0800

    feat: add day10

---
+++ src/routes/todo-list/Input.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	interface InputProps {
+		label: string;
+		value: string;
+	}
+	let { label, value = $bindable() }: InputProps = $props();
+</script>
+
+<label class="form-control w-full">
+	<div class="label">
+		<span class="label-text text-primary">{label}</span>
+	</div>
+	<input type="text" class="input input-bordered w-full" bind:value />
+</label>
---
+++ src/routes/todo-list/PageLayout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	interface PageLayoutProps {
+		children: Snippet;
+	}
+	let { children }: PageLayoutProps = $props();
+</script>
+
+<div class="max-w-3xl mx-auto">
+	{@render children()}
+</div>
