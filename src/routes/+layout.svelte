9fb1a9ba09470da6083de18375f4c664cfa53226
ToddLiao <toddliao.dev@gmail.com>
2024-10-01 23:41:04 +0800

    feat: add day17

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,12 @@
 	});
 </script>
 
+<nav>
+	<a href="/">Home</a>
+	<a href="/day16">day16</a>
+	<a href="/day17">day17</a>
+</nav>
+
 <QueryClientProvider client={queryClient}>
 	<slot />
 </QueryClientProvider>
---
+++ src/routes/day17/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/day16">day16</a>
+</nav>
+
+<slot />
