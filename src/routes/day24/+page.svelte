9027aa156797dceb36aadf1507027d7a46e9b87b
ToddLiao <toddliao.dev@gmail.com>
2024-10-08 23:35:38 +0800

    feat: add day24 (#16)

---
+++ src/routes/day24/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Snapshot } from './$types';
+
+	let formData = $state({
+		email: '',
+		name: ''
+	});
+
+	export const snapshot: Snapshot<typeof formData> = {
+		capture: () => formData,
+		restore: (value) => (formData = value)
+	};
+
+	$effect(() => {
+		console.log('formData', formData, history);
+	});
+</script>
+
+<label>
+	Email:
+	<input id="email" bind:value={formData.email} />
+</label>
+<label>
+	Name:
+	<input id="name" bind:value={formData.name} />
+</label>
